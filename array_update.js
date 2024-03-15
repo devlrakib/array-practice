1de68cc3e3e2826b45eebe35682084063cb57e15
devlrakib <devrakib5@gmail.com>
2024-03-15 19:24:35 +0600

    first commit

---
+++ array_update.js
@@ -0,0 +1,10 @@
+
+// ! Javascript h.w 1
+var fruits = [ 'Apple', 'Banana', 'Orange'];
+var fruitsName = fruits.indexOf('Banana');
+console.log(fruitsName);
+ fruits.pop();
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
+
